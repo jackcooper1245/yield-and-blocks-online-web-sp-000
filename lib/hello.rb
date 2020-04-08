aa6b2caa1fda8e76a70b11646420f1ddf962c45c
Jack Cooper <jackcooper1245@gmail.com>
2020-04-08 09:12:15 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -7,12 +7,8 @@ i += 1
 end
 end
 
-def hello_t(["Tim", "Tom", "Jim"])
-i = 0
-
-while i < array.length
-yield array[i]
-i += 1
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
 end
-end
-
